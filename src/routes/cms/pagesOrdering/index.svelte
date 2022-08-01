6ced034d4dd9d3e77134f8c445fe96cd04d4d925
LeVarez <aleixac1997@gmail.com>
2022-08-01 12:58:06 +0200

    pageordering index page

---
+++ src/routes/cms/pagesOrdering/index.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import type { SubTypes } from "$lib/types";
+
+  export let pages: SubTypes.PageOrdering.PageOrdering[];
+
+</script>
+
+<div class="container">
+  {#each pages as page}
+    <div class="item">
+      <a class="name" href={"/cms/pagesOrdering/" + page.id}>{page.name}</a>
+    </div>
+  {/each}
+</div>
+
+<style lang="stylus">
+  .container{
+    cursor: pointer;
+    width: 50%;
+    margin: 0 auto;
+
+  }
+  .item{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+  }
+</style>
