fcd14855a08ff15667225405eecbca3d6cf3a800
MKenz0 <miguel7vj@gmail.com>
2022-09-05 13:11:04 +0200

    Update +page.svelte

--- packages/web/src/routes/terms-of-use/+page.svelte
+++ packages/web/src/routes/terms-of-use/+page.svelte
@@ -2,7 +2,7 @@
   import SplashStatic from "$lib/components/generic/SplashStatic.svelte";
 </script>
 
-<SplashStatic page="Terms of use"/>
+<SplashStatic page="Terms of use" subtitle="As of August 24, 2022"/>
 
 <style lang="stylus">
 
