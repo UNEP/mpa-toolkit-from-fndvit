f2ad6a6b354149ecc9d4043f69ba79bbe5c1b2b9
MKenz0 <miguel7vj@gmail.com>
2022-09-07 17:24:17 +0200

    Remove svelte-markdown & vite-plugin-markdown

--- packages/web/src/routes/privacy-policy/+page.server.ts
+++ packages/web/src/routes/privacy-policy/+page.server.ts
@@ -1,11 +1,5 @@
-import { marked } from 'marked';
 import type { PageServerLoad } from './$types';
-import content from './content.json';
-import { html } from './testA.md';
 
-
-export const load: PageServerLoad = async ({ locals, url }) => {
-  const texto = marked.parse('# Marked in Node.js\n\nRendered by **marked**.');
-  console.log({texto});
-  return {content};
+export const load: PageServerLoad = async ({ locals }) => {
+  return {};
 };
