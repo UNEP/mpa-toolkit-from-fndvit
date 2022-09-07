1ca4e6c028ecc091d22f4af26cbb3c0c605e9ede
MKenz0 <miguel7vj@gmail.com>
2022-09-07 13:10:37 +0200

    use marked

--- packages/web/src/routes/privacy-policy/+page.server.ts
+++ packages/web/src/routes/privacy-policy/+page.server.ts
@@ -1,8 +1,11 @@
-import { Queries } from '@mpa/db';
+import { marked } from 'marked';
 import type { PageServerLoad } from './$types';
-import { db } from '$lib/db';
 import content from './content.json';
+import { html } from './testA.md';
+
 
 export const load: PageServerLoad = async ({ locals, url }) => {
+  const texto = marked.parse('# Marked in Node.js\n\nRendered by **marked**.');
+  console.log({texto});
   return {content};
 };
