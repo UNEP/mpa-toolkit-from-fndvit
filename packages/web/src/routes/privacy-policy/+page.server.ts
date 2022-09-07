3c3ccf184dbb692eedfc2a895144c33ee9eca40a
MKenz0 <miguel7vj@gmail.com>
2022-09-07 17:27:14 +0200

    Remove markdown vite config

--- packages/web/src/routes/privacy-policy/+page.server.ts
+++ packages/web/src/routes/privacy-policy/+page.server.ts
@@ -1,4 +1,8 @@
+import { Queries } from '@mpa/db';
+import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { slugify } from '$lib/utils';
+import { db } from '$lib/db';
 
 export const load: PageServerLoad = async ({ locals }) => {
   return {};
