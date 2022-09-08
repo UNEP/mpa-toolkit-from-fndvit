2639f3185eefc3bda765c8863b65ec93f70c6e46
MKenz0 <miguel7vj@gmail.com>
2022-09-08 16:49:58 +0200

    Remove image in error page

--- packages/web/src/routes/+error.svelte
+++ packages/web/src/routes/+error.svelte
@@ -1,19 +1,14 @@
 <script>
   import { page } from '$app/stores';
-  import turtle from '$lib/assets/error-default-image.png';
 </script>
 
 <div class="container">
   <h1>{$page.status} error</h1>
   {#if $page.status === 404}
-    <div class="inner">
-      <p>Erm ...<br>That's a bit awkward ...<br>But ...<br>We couldn't find that page<br>Try going to <a href="/">MPAth's home page</a>.</p>
-      <div class="img" style="background-image: url({turtle})"/>
-    </div>
+    <p>Erm ...<br>That's a bit awkward ...<br>But ...<br>We couldn't find that page<br>Try going to <a href="/">MPAth's home page</a>.</p>
   {:else}
     <div class="inner">
-      <p>Ooops!!<br>This one's one us — something's not right on our side.<br>Refresh your browser or try us later maybe?</p>
-      <div class="img" style="background-image: url({turtle})"/>
+      <p>Ooops!!<br>This one's one us <br>— something's not right on our side.<br>Refresh your browser or try us later maybe?</p>
     </div>
   {/if}
 </div>
@@ -36,25 +31,6 @@
     a {
       font-weight: bold;
       color: $colors.neutral-dark;
-    }
-  }
-
-  .img {
-    width: 448px;
-    height: 408px;
-    filter: grayscale(100%) brightness(1.1);
-    transform: translate(-45px, -100px);
-  }
-
-  .inner {
-    display: flex;
-    flex-direction: row;
-  }
-
-  +breakpoint(page, medium){
-
-    .inner {
-      flex-direction: column;
     }
   }
 
